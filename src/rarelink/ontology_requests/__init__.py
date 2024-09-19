e337dd681618d7e55a0be1315eaca8849f9ed485
frehburg <fil.rehburg@gmail.com>
2024-09-19 12:38:05 +0200

    added ontology api requests module

---
+++ src/rarelink/ontology_requests/__init__.py
@@ -0,0 +1,6 @@
+"""This module is a collection of methods that take a code and make an api request to the ontology to retrieve more
+information about the concept"""
+
+__all__ = [
+
+]
---
+++ src/rarelink/ontology_requests/ncit_api_request.py
@@ -0,0 +1,6 @@
+from phenopacket_mapper.data_standards import Coding
+
+
+def mondo_api_request(code: str) -> Coding:
+    """Request a term from the Monarch Initiative API"""
+    raise NotImplementedError
