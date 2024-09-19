3c10fcdce94905cc6ef753b6427c0c901fc17e7a
frehburg <fil.rehburg@gmail.com>
2024-09-19 16:11:19 +0200

    uncommented a few lines

--- src/rarelink/pipelines/redcap_phenopacket_pipeline/phenopacket_pipeline.py
+++ src/rarelink/pipelines/redcap_phenopacket_pipeline/phenopacket_pipeline.py
@@ -10,9 +10,8 @@ from rarelink.preprocessing import preprocess_redcap_for_phenopacket_pipeline
 
 def phenopacket_pipeline(path: Union[str, Path], rarelink_data_model=RARELINK_DATAMODEL_2_0) -> List[Phenopacket]:
     # 0. load data using data model
-    # rl_data_set = rarelink_data_model.load_data(path)  # TODO: once implemented in phenopacket_mapper,
-                                                        #  TODO: uncomment this line
+    rl_data_set = rarelink_data_model.load_data(path)
     # 1. preprocessing
-    # df = preprocess_redcap_for_phenopacket_pipeline(rl_data_set)  # TODO: uncomment
+    df = preprocess_redcap_for_phenopacket_pipeline(rl_data_set)  # TODO: uncomment
     # 2. mapping
     raise NotImplementedError
