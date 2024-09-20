dd8b2fb56da20c093ad7c913cf94535e483d8560
graefea <adam.graefe@charite.de>
2024-09-20 14:09:32 +0200

    addded

--- tests/preprocessing/test_preprocess_redcap_code.py
+++ tests/preprocessing/test_preprocess_redcap_code.py
@@ -17,7 +17,7 @@ def resources():
     #    CodeSystem(name='National Cancer Institute Thesaurus (NCIT)', namespace_prefix='NCIT', url='https://ncithesaurus.org/'),
     #    CodeSystem(name='Sequence Ontology (SO)', namespace_prefix='SO', url='http://www.sequenceontology.org/'),
         CodeSystem(name='International Classification of Diseases', namespace_prefix='ICD10CM', url='https://www.cdc.gov/nchs/icd/icd10cm.htm'),
-    #    CodeSystem(name='ICD-11', namespace_prefix='ICD11', url='https://icd.who.int/en')
+        CodeSystem(name='ICD-11', namespace_prefix='ICD11', url='https://icd.who.int/en')
     ]
 
 @pytest.mark.parametrize("input, expected", [
@@ -34,7 +34,8 @@ def resources():
 #    ("ncit_00000", Coding(system=CodeSystem(name='National Cancer Institute Thesaurus (NCIT)', namespace_prefix='NCIT', url='https://ncithesaurus.org/'), code='00000')),
 #    ("so_00000", Coding(system=CodeSystem(name='Sequence Ontology (SO)', namespace_prefix='SO', url='http://www.sequenceontology.org/'), code='00000')),
     ("icd10cm_R51_1", Coding(system=CodeSystem(name='International Classification of Diseases', namespace_prefix='ICD10CM', url='https://www.cdc.gov/nchs/icd/icd10cm.htm'), code='R51.1')),
-    ("icd11_r51_2", Coding(system=CodeSystem(name='ICD-11', namespace_prefix='ICD11', url='https://icd.who.int/en'), code='ICD11:R51.2')),
+    ("icd10_r51_2", Coding(system=CodeSystem(name='International Classification of Diseases', namespace_prefix='ICD10', url='https://icd.who.int/en'), code='ICD10:R51.2')),
+ #   ("icd11_r51_2", Coding(system=CodeSystem(name='ICD-11', namespace_prefix='ICD11', url='https://icd.who.int/en'), code='ICD11:R51.2')),
 
 ])
 def test_preprocess_redcap_code(input, resources, expected):
