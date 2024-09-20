bf108f96ba578d83bbeb6f9cd1ee7a147bfe8510
graefea <adam.graefe@charite.de>
2024-09-20 13:23:22 +0200

    changed eample code

--- src/rarelink/preprocessing/preprocess_redcap_codes.py
+++ src/rarelink/preprocessing/preprocess_redcap_codes.py
@@ -6,14 +6,14 @@ def parse_redcap_code(redcap_code: str, resources: List[CodeSystem]) -> Coding:
     """
     This method parses a string representation of a code in the REDCap format to a Coding object.
     
-    :param redcap_code: A string with format like 'snomed_12345', 'mondo_125', or any prefix followed by '_'
+    :param redcap_code: A string with format like 'snomed_1234567', 'mondo_1234567', or any prefix followed by '_'
                         and the code.
     :param resources: A list of available CodeSystems that the redcap_code prefix can be matched to.
     :return: A Coding object with the matching CodeSystem and the extracted code.
     
     Example:
-    >>> parse_redcap_code("snomed_12345", [CodeSystem(name="SNOMED CT", namespace_prefix="SNOMED")])
-    Coding(system=CodeSystem(name='SNOMED CT', namespace_prefix='SNOMED'), code='12345')
+    >>> parse_redcap_code("snomed_1234567", [CodeSystem(name="SNOMED CT", namespace_prefix="SNOMED")])
+    Coding(system=CodeSystem(name='SNOMED CT', namespace_prefix='SNOMED'), code='1234567')
     """
 
     # Split the redcap_code into its prefix and code part
