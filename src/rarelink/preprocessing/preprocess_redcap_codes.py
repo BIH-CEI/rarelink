61e3a766c919c23b212d28fdf7cad7c6a4168c3c
graefea <adam.graefe@charite.de>
2024-09-20 13:47:01 +0200

    added LOINC speciality of REDCap codes

--- src/rarelink/preprocessing/preprocess_redcap_codes.py
+++ src/rarelink/preprocessing/preprocess_redcap_codes.py
@@ -1,19 +1,18 @@
 from typing import List
 from phenopacket_mapper.data_standards import Coding, CodeSystem
 
-
 def parse_redcap_code(redcap_code: str, resources: List[CodeSystem]) -> Coding:
     """
     This method parses a string representation of a code in the REDCap format to a Coding object.
     
     :param redcap_code: A string with format like 'snomed_1234567', 'mondo_1234567', or any prefix followed by '_'
-                        and the code.
+                        and the code. Special case: 'loinc_81304_8' should convert the underscore in the code part to a hyphen.
     :param resources: A list of available CodeSystems that the redcap_code prefix can be matched to.
     :return: A Coding object with the matching CodeSystem and the extracted code.
     
     Example:
     # parse_redcap_code("snomed_1234567", [CodeSystem(name="SNOMED CT", namespace_prefix="SNOMED")])
-    Coding(system=CodeSystem(name='SNOMED CT', namespace_prefix='SNOMED'), code='1234567')
+    # Coding(system=CodeSystem(name='SNOMED CT', namespace_prefix='SNOMED'), code='1234567')
     """
 
     # Split the redcap_code into its prefix and code part
@@ -21,6 +20,10 @@ def parse_redcap_code(redcap_code: str, resources: List[CodeSystem]) -> Coding:
         prefix, code = redcap_code.split('_', 1)
     except ValueError:
         raise ValueError(f"Invalid format for redcap_code: {redcap_code}. Expected format: 'prefix_code'.")
+
+    # If the prefix is 'loinc', replace any '_' in the code part with '-'
+    if prefix.lower() == "loinc":
+        code = code.replace('_', '-')
 
     # Find the matching CodeSystem based on the prefix
     for resource in resources:
--- tests/preprocessing/test_preprocess_redcap_code.py
+++ tests/preprocessing/test_preprocess_redcap_code.py
@@ -22,17 +22,17 @@ def resources():
 
 @pytest.mark.parametrize("input, expected", [
     ("snomed_410605003", Coding(system=CodeSystem(name='SNOMED CT', namespace_prefix='SNOMED', url='https://www.snomed.org/snomed-ct'), code='410605003')),
-    ("mondo_0968976", Coding(system=CodeSystem(name='MONDO', namespace_prefix='MONDO', url='https://mondo.monarchinitiative.org/'), code='0968976')),
+    ("mondo_0968976", Coding(system=CodeSystem(name='Monarch Disease Ontology', namespace_prefix='MONDO', url='https://mondo.monarchinitiative.org/'), code='0968976')),
     ("hp_4000034", Coding(system=CodeSystem(name='Human Phenotype Ontology', namespace_prefix='HP', url='https://hpo.jax.org/'), code='4000034')),
-    ("omim_147920", Coding(system=CodeSystem(name='OMIM', namespace_prefix='OMIM', url='https://omim.org/'), code='147920')),
-    ("ordo_2322", Coding(system=CodeSystem(name='ORDO', namespace_prefix='ORDO', url='https://www.orpha.net/consor/cgi-bin/index.php'), code='2322')),
+    ("omim_147920", Coding(system=CodeSystem(name='Online Mendelian Inheritance', namespace_prefix='OMIM', url='https://omim.org/'), code='147920')),
+    ("ordo_2322", Coding(system=CodeSystem(name='Orphanet', namespace_prefix='ORDO', url='https://www.orpha.net/consor/cgi-bin/index.php'), code='2322')),
 #    ("ncbitaxon_399550", Coding(system=CodeSystem(name='NCBI organismal classification', namespace_prefix='NCBITAXON', url='https://www.ncbi.nlm.nih.gov/taxonomy'), code='399550')),
-    ("loinc_81304_8", Coding(system=CodeSystem(name='LOINC', namespace_prefix='LOINC', url='https://loinc.org/'), code='81304-8')),
-    ("hgnc_00000", Coding(system=CodeSystem(name='HGNC', namespace_prefix='HGNC', url='https://www.genenames.org/'), code='00000')),
+    ("loinc_81304_8", Coding(system=CodeSystem(name='Logical Observation Identifiers Names and Codes', namespace_prefix='LOINC', url='https://loinc.org/'), code='81304-8')),
+    ("hgnc_00000", Coding(system=CodeSystem(name='HUGO Gene Nomenclature Committee', namespace_prefix='HGNC', url='https://www.genenames.org/'), code='00000')),
 #    ("geno_00000", Coding(system=CodeSystem(name='GENO: The Genotype Ontology', namespace_prefix='GENO', url='http://www.genoontology.org/'), code='00000')),
 #    ("ncit_00000", Coding(system=CodeSystem(name='National Cancer Institute Thesaurus (NCIT)', namespace_prefix='NCIT', url='https://ncithesaurus.org/'), code='00000')),
 #    ("so_00000", Coding(system=CodeSystem(name='Sequence Ontology (SO)', namespace_prefix='SO', url='http://www.sequenceontology.org/'), code='00000')),
-    ("icd10cm_00000", Coding(system=CodeSystem(name='ICD-10-CM', namespace_prefix='ICD10CM', url='https://www.cdc.gov/nchs/icd/icd10cm.htm'), code='00000')),
+    ("icd10cm_00000", Coding(system=CodeSystem(name='International Classification of Diseases', namespace_prefix='ICD10CM', url='https://www.cdc.gov/nchs/icd/icd10cm.htm'), code='00000')),
 #    ("icd11_00000", Coding(system=CodeSystem(name='ICD-11', namespace_prefix='ICD11', url='https://icd.who.int/en'), code='00000')),
 ])
 def test_preprocess_redcap_code(input, resources, expected):
