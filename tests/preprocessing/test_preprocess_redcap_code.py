ce0d23694728d1b9b43a3f0bbdfa16f20226461b
graefea <adam.graefe@charite.de>
2024-09-20 13:29:06 +0200

    added tests for all ontologies used.

--- tests/preprocessing/test_preprocess_redcap_code.py
+++ tests/preprocessing/test_preprocess_redcap_code.py
@@ -1,19 +1,39 @@
 import pytest
-
 from rarelink.preprocessing import parse_redcap_code
-
+from phenopacket_mapper.data_standards import CodeSystem, Coding
 
 @pytest.fixture
 def resources():
     return [
-        "CodeSystem(name='SNOMED CT', namespace_prefix='SNOMED')",
-        "CodeSystem(name='MONDO', namespace_prefix='MONDO')",
+        CodeSystem(name='SNOMED CT', namespace_prefix='SNOMED', url='https://www.snomed.org/snomed-ct'),
+        CodeSystem(name='MONDO', namespace_prefix='MONDO', url='https://mondo.monarchinitiative.org/'),
+        CodeSystem(name='HPO', namespace_prefix='HPO', url='https://hpo.jax.org/'),
+        CodeSystem(name='OMIM', namespace_prefix='OMIM', url='https://omim.org/'),
+        CodeSystem(name='ORDO', namespace_prefix='ORDO', url='https://www.orpha.net/consor/cgi-bin/index.php'),
+        CodeSystem(name='NCBI Taxonomy', namespace_prefix='NCBITAXON', url='https://www.ncbi.nlm.nih.gov/taxonomy'),
+        CodeSystem(name='LOINC', namespace_prefix='LOINC', url='https://loinc.org/'),
+        CodeSystem(name='HGNC', namespace_prefix='HGNC', url='https://www.genenames.org/'),
+        CodeSystem(name='GENO: The Genotype Ontology', namespace_prefix='GENO', url='http://www.genoontology.org/'),
+        CodeSystem(name='National Cancer Institute Thesaurus (NCIT)', namespace_prefix='NCIT', url='https://ncithesaurus.org/'),
+        CodeSystem(name='Sequence Ontology (SO)', namespace_prefix='SO', url='http://www.sequenceontology.org/'),
+        CodeSystem(name='ICD-10-CM', namespace_prefix='ICD10CM', url='https://www.cdc.gov/nchs/icd/icd10cm.htm'),
+        CodeSystem(name='ICD-11', namespace_prefix='ICD11', url='https://icd.who.int/en')
     ]
 
 @pytest.mark.parametrize("input, expected", [
-    ("snomed_410605003", "Coding(system=CodeSystem(name='SNOMED CT', namespace_prefix='SNOMED'), code='410605003')"),
-    ("mondo_0968976", "Coding(system=CodeSystem(name='MONDO', namespace_prefix='MONDO'), code='0968976')"),
+    ("snomed_410605003", Coding(system=CodeSystem(name='SNOMED CT', namespace_prefix='SNOMED', url='https://www.snomed.org/snomed-ct'), code='410605003')),
+    ("mondo_0968976", Coding(system=CodeSystem(name='MONDO', namespace_prefix='MONDO', url='https://mondo.monarchinitiative.org/'), code='0968976')),
+    ("hpo_00000", Coding(system=CodeSystem(name='Human Phenotype Ontology', namespace_prefix='HPO', url='https://hpo.jax.org/'), code='00000')),
+    ("omim_00000", Coding(system=CodeSystem(name='OMIM', namespace_prefix='OMIM', url='https://omim.org/'), code='00000')),
+    ("ordo_00000", Coding(system=CodeSystem(name='ORDO', namespace_prefix='ORDO', url='https://www.orpha.net/consor/cgi-bin/index.php'), code='00000')),
+    ("ncbitaxon_00000", Coding(system=CodeSystem(name='NCBI Taxonomy', namespace_prefix='NCBITAXON', url='https://www.ncbi.nlm.nih.gov/taxonomy'), code='00000')),
+    ("loinc_00000", Coding(system=CodeSystem(name='LOINC', namespace_prefix='LOINC', url='https://loinc.org/'), code='00000')),
+    ("hgnc_00000", Coding(system=CodeSystem(name='HGNC', namespace_prefix='HGNC', url='https://www.genenames.org/'), code='00000')),
+    ("geno_00000", Coding(system=CodeSystem(name='GENO: The Genotype Ontology', namespace_prefix='GENO', url='http://www.genoontology.org/'), code='00000')),
+    ("ncit_00000", Coding(system=CodeSystem(name='National Cancer Institute Thesaurus (NCIT)', namespace_prefix='NCIT', url='https://ncithesaurus.org/'), code='00000')),
+    ("so_00000", Coding(system=CodeSystem(name='Sequence Ontology (SO)', namespace_prefix='SO', url='http://www.sequenceontology.org/'), code='00000')),
+    ("icd10cm_00000", Coding(system=CodeSystem(name='ICD-10-CM', namespace_prefix='ICD10CM', url='https://www.cdc.gov/nchs/icd/icd10cm.htm'), code='00000')),
+    ("icd11_00000", Coding(system=CodeSystem(name='ICD-11', namespace_prefix='ICD11', url='https://icd.who.int/en'), code='00000')),
 ])
 def test_preprocess_redcap_code(input, resources, expected):
     assert parse_redcap_code(input, resources) == expected
-
