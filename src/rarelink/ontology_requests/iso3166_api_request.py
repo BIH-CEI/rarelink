bf2b17d7cd2d93fd2debf82fd36de84f8a900d21
Adam SL Graefe <109136019+aslgraefe@users.noreply.github.com>
2024-09-21 16:11:07 +0200

    Add test (#54)
    * test new
    
    * typo
    
    * update phenopacket_mapper
    
    * edit
    
    * edit
    
    * edit
    
    * added function for prerpocess redcap code
    
    * changed eample code
    
    * added tests for all ontologies used.
    
    * changed names
    
    * commented out codesystems missing for now
    
    * fix
    
    * test
    
    * added testing codes
    
    * fix
    
    * fix
    
    * added LOINC speciality of REDCap codes
    
    * added icd10 code speciality ,and checking for bioportal format
    
    * added icd10 example
    
    * added more special cases for ICD codes
    
    * edited tests
    
    * updated test
    
    * addded
    
    * added icd10
    
    * fix
    
    * fix
    
    * fixed icd10 issue
    
    * changed name to rarelink-cdm
    
    * changed name
    
    * added iso3166 codes
    
    * added iso3166 request py
    
    * changed name
    
    * deleted not mappabale elements of rd cdm
    
    * added hl78 fhir to testing
    
    * to notebook changed coding of elements with vss
    
    * added hl7 fhir coding
    
    * fix
    
    * added hl7 fhir and verison
    
    * added
    
    * added ga4gh
    
    * added ga4gh code
    
    * added 6.1 genetic findings
    
    * added genetic findgins
    
    * changed data element descriptions
    
    * edit family history
    
    * added data model definitions to notebook
    
    * changed name of notebook
    
    * generated rarelink_cdm data model
    
    * printed rarelink_cdm
    
    * ae
    
    * renamed notebook
    
    * changed names to rarelink cdm
    
    * fix
    
    * edit
    
    * fixed versions of ontologies
    
    * fix
    
    * fixed name
    
    * fix name
    
    ---------
    
    Co-authored-by: frehburg <fil.rehburg@gmail.com>

---
+++ src/rarelink/ontology_requests/iso3166_api_request.py
@@ -0,0 +1,6 @@
+from phenopacket_mapper.data_standards import Coding
+
+
+def mondo_api_request(code: str) -> Coding:
+    """Request a term from the ISO 3166 API"""
+    raise NotImplementedError
--- src/rarelink/pipelines/rarelink_data_model.py
+++
@@ -1,8 +0,0 @@
-from phenopacket_mapper.data_standards import DataModel, DataField
-from phenopacket_mapper.data_standards.value_set import ValueSet
-
-RARELINK_DATAMODEL_2_0 = DataModel(
-    data_model_name="rarelink",
-    fields=[DataField(name="temp", value_set=ValueSet())],
-    resources=[]
-)  # TODO @aslgrafe: replace with finished datamodel
