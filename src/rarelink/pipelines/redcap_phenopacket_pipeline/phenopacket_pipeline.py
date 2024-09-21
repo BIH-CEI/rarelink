2a57bd1c088778ca8e8150cdb8454db4be3eb577
graefea <adam.graefe@charite.de>
2024-09-21 18:39:19 +0200

    fix

--- src/rarelink/pipelines/redcap_phenopacket_pipeline/phenopacket_pipeline.py
+++ src/rarelink/pipelines/redcap_phenopacket_pipeline/phenopacket_pipeline.py
@@ -10,8 +10,8 @@ from rarelink.preprocessing import preprocess_redcap_for_phenopacket_pipeline
 
 def phenopacket_pipeline(path: Union[str, Path], rarelink_data_model=rarelink_cdm_v2_0_0) -> List[Phenopacket]:
     # 0. load data using data model
-    rl_data_set = rarelink_data_model.load_data(path)
+   # rl_data_set = rarelink_data_model.load_data(path)
     # 1. preprocessing
-    df = preprocess_redcap_for_phenopacket_pipeline(rl_data_set)  # TODO: uncomment
+   # df = preprocess_redcap_for_phenopacket_pipeline(rl_data_set)  # TODO: uncomment
     # 2. mapping
     raise NotImplementedError
