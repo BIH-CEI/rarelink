b7eb1e8a1e6b51c55ed3efc2e5331ce83925fe2a
graefea <adam.graefe@charite.de>
2024-09-21 18:58:48 +0200

    fix

--- src/rarelink/rarelink_cdm/__init__.py
+++ src/rarelink/rarelink_cdm/__init__.py
@@ -7,10 +7,10 @@ __all__ = [
     "rarelink_cdm_v2_0_0_codesystems"
 ]
 
-def initialize():
-    from . import rarelink_cdm_v2_0_0
-    from . import rarelink_cdm_v2_0_0_vs
-    from . import rarelink_cdm_v2_0_0_codesystems
+# def initialize():
+#     from . import rarelink_cdm_v2_0_0
+#     from . import rarelink_cdm_v2_0_0_vs
+#     from . import rarelink_cdm_v2_0_0_codesystems
 
-initialize()
+# initialize()
 
