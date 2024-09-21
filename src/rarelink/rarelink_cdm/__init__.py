b495c7f9c78add448be24134ba42ce20a77ef931
graefea <adam.graefe@charite.de>
2024-09-21 18:04:21 +0200

    updated init in that folder

--- src/rarelink/rarelink_cdm/__init__.py
+++ src/rarelink/rarelink_cdm/__init__.py
@@ -1,9 +1,12 @@
 """This module includes the RareLink Common Data Model and its value sets with the resepctive versions.""" 
 
-from . import rarelink_cdm_2_0
-from . import rarelink_cdm_vs_2_0
+from . import rarelink_cdm_v2_0_0
+from . import rarelink_cdm_v2_0_0_vs
+from . import rarelink_cdm_v2_0_0_codesystems
 
 __all__ = [
-    "rarelink_cdm_2_0",
-    "rarelink_cdm_vs_2_0",
+    "rarelink_cdm_v2_0_0",
+    "rarelink_cdm_v2_0_0_vs",
+    "rarelink_cdm_v2_0_0_codesystems"
 ]
+
