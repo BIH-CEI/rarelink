efb24aced147035c0e0dc0c04cb15071dfa4b9fe
graefea <adam.graefe@charite.de>
2024-09-21 19:00:17 +0200

    fix

--- src/rarelink/__init__.py
+++ src/rarelink/__init__.py
@@ -3,7 +3,7 @@ Registry Use, HL7 FHIR and GA4GH Phenopackets"""
 
 __version__ = "0.0.1"
 
-from . import example, numpy_example
+from . import numpy_example
 from . import ontology_requests
 from . import preprocessing
 from . import pipelines
