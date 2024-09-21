7d20d41bbd3bf347796e798a056bff61c4803aa7
graefea <adam.graefe@charite.de>
2024-09-21 12:42:35 +0200

    fix

--- src/rarelink/pipelines/rarelink_cdm.py
+++ src/rarelink/pipelines/rarelink_cdm.py
@@ -1,7 +1,7 @@
 from phenopacket_mapper.data_standards import DataModel, DataField
 from phenopacket_mapper.data_standards.value_set import ValueSet
 
-RARELINK_DATAMODEL_2_0 = DataModel(
+RARELINK_CDM_2_0 = DataModel(
     data_model_name="rarelink_cdm",
     fields=[DataField(name="temp", value_set=ValueSet())],
     resources=[]
