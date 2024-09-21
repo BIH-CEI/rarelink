d18d40581a786543017ac449bbe2036df5d8b689
graefea <adam.graefe@charite.de>
2024-09-21 12:50:31 +0200

    fix name

--- src/rarelink/pipelines/redcap_phenopacket_pipeline/phenopacket_pipeline.py
+++ src/rarelink/pipelines/redcap_phenopacket_pipeline/phenopacket_pipeline.py
@@ -3,12 +3,12 @@ from typing import Union, List
 
 from phenopackets.schema.v2 import Phenopacket
 
-from rarelink.pipelines import RARELINK_CDM_2_0
+from rarelink.pipelines import rarelink_cdm_2_0
 
 from rarelink.preprocessing import preprocess_redcap_for_phenopacket_pipeline
 
 
-def phenopacket_pipeline(path: Union[str, Path], rarelink_data_model=RARELINK_CDM_2_0) -> List[Phenopacket]:
+def phenopacket_pipeline(path: Union[str, Path], rarelink_data_model=rarelink_cdm_2_0) -> List[Phenopacket]:
     # 0. load data using data model
     # rl_data_set = rarelink_data_model.load_data(path)  # TODO: once implemented in phenopacket_mapper,
                                                         #  TODO: uncomment this line
