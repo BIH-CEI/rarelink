de51a3310f4a50ee8b550dff535fbad8aad72522
graefea <adam.graefe@charite.de>
2024-09-21 12:49:51 +0200

    fixed name

--- src/rarelink/pipelines/__init__.py
+++ src/rarelink/pipelines/__init__.py
@@ -1,13 +1,13 @@
 """This module includes the codes for the different pipelines"""
 
-from .rarelink_cdm import RARELINK_CDM_2_0
+from .rarelink_cdm import rarelink_cdm_2_0
 from . import redcap_fhir_pipeline
 from . import redcap_phenopacket_pipeline
 from . import tabular_redcap_pipeline
 
 __all__ = [
 
-    "RARELINK_CDM_2_0",
+    "rarelink_cdm_2_0",
     "redcap_fhir_pipeline",
     "redcap_phenopacket_pipeline",
     "tabular_redcap_pipeline",
