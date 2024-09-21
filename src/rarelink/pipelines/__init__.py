7ef80fc4374a917374d373af7a40f401a84f5220
graefea <adam.graefe@charite.de>
2024-09-21 12:41:38 +0200

    changed names to rarelink cdm

--- src/rarelink/pipelines/__init__.py
+++ src/rarelink/pipelines/__init__.py
@@ -1,13 +1,13 @@
 """This module includes the codes for the different pipelines"""
 
-from .rarelink_data_model import RARELINK_DATAMODEL_2_0
+from .rarelink_cdm import RARELINK_CDM_2_0
 from . import redcap_fhir_pipeline
 from . import redcap_phenopacket_pipeline
 from . import tabular_redcap_pipeline
 
 __all__ = [
 
-    "RARELINK_DATAMODEL_2_0",
+    "RARELINK_CDM_2_0",
     "redcap_fhir_pipeline",
     "redcap_phenopacket_pipeline",
     "tabular_redcap_pipeline",
--- src/rarelink/pipelines/tabular_redcap_pipeline/__init__.py
+++ src/rarelink/pipelines/tabular_redcap_pipeline/__init__.py
@@ -1,4 +1,4 @@
-"""This module implements the pipeline from any tabular medical data set to the RareLink REDCap CDM."""
+"""This module implements the pipeline from any tabular medical data set to the RareLink CDM."""
 
 __all__ = [
 
