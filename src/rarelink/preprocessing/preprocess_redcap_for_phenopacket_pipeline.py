9b805508327d1a635f9abd545323aeaaf54e92c3
graefea <adam.graefe@charite.de>
2024-09-23 22:01:26 +0200

    added todo for preproccess redcap for phenopacket pipeline function

--- src/rarelink/preprocessing/preprocess_redcap_for_phenopacket_pipeline.py
+++ src/rarelink/preprocessing/preprocess_redcap_for_phenopacket_pipeline.py
@@ -1,6 +1,9 @@
 
 
 import pandas as pd
+from pathlib import Path
+from typing import Union
+
 
 
 class DataSet:
@@ -8,11 +11,21 @@ class DataSet:
     pass
 
 
-def preprocess_redcap_for_phenopacket_pipeline(ds: DataSet) -> pd.DataFrame:
+def preprocess_redcap_for_phenopacket_pipeline(
+        input_path: Union[str, Path], 
+        output_path: Union[str, Path]
+) -> Union[str, Path]:
     """Preprocess REDCap data for the phenopacket pipeline.
 
     :param ds: A pandas DataFrame with REDCap data loaded in the Rarelink data model.
     """
     # TODO @aslgrafe: handle ds.data_frame preprocessing
-    # df = ds.data_frame
+    # 1. read csv file from inputpath
+    # 2. preprocess the data frame
+    # 3. save the preprocessed data frame to output path
+    # 4. return the output path
     raise NotImplementedError
+
+
+# product of this function is the preprocessed data frame saved a csv or what ever to be used by the phenopaxcket pipeline.py 
+# upload data -> preprocess -> output locally -> output of this function: path of precprocessed data
