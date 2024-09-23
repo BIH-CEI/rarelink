fe8a0fe77a4cc7cdc631faea23af8d8d021a0f1d
graefea <adam.graefe@charite.de>
2024-09-23 22:12:32 +0200

    fiox

--- src/rarelink/rarelink_cdm/__init__.py
+++ src/rarelink/rarelink_cdm/__init__.py
@@ -2,13 +2,13 @@
 with the resepctive versions.""" 
 
 from .rarelink_cdm_codesystems import RARELINK_CDM_V2_0_0_RESOURCES
-from .rarelink_cdm import rarelink_cdm_v2_0_0
-from .rarelink_cdm import rarelink_cdm_v2_0_0_vs
+from .rarelink_cdm import RARELINK_CDM_V2_0_0
+from .rarelink_cdm import RARELINK_CDM_V2_0_0_VS
 from .rarelink_cdm import load_rarelink_data
 
 __all__ = [
-    "rarelink_cdm_v2_0_0", 
-    "rarelink_cdm_v2_0_0_vs",
+    "RARELINK_CDM_V2_0_0",
+    "RARELINK_CDM_V2_0_0_VS",
     "RARELINK_CDM_V2_0_0_RESOURCES",
     "load_rarelink_data"
 ]
