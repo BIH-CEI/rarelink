2f3d7b9d685a70d25b05402c945215610d9f690b
graefea <adam.graefe@charite.de>
2024-09-24 22:24:09 +0200

    updated name

--- src/rarelink/preprocessing/__init__.py
+++ src/rarelink/preprocessing/__init__.py
@@ -1,9 +1,9 @@
 """This module implements utility functions to preprocess medical datasets in tabular formats."""
 
-from .preprocess_redcap_for_phenopacket import preprocess_redcap_for_phenopacket_pipeline
+from .preprocess_redcap_for_phenopackets import preprocess_redcap_for_phenopackets
 from .preprocess_redcap_codes import parse_redcap_code
 
 __all__ = [
-    "preprocess_redcap_for_phenopacket",
+    "preprocess_redcap_for_phenopackets",
     "parse_redcap_code",
 ]
