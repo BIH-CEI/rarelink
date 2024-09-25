aca4ed147f6bf6b78e6e4700c8e1ce5dde34a775
graefea <adam.graefe@charite.de>
2024-09-25 12:19:41 +0200

    fix

--- src/rarelink/rarelink_cdm/__init__.py
+++ src/rarelink/rarelink_cdm/__init__.py
@@ -5,8 +5,8 @@ from .rarelink_cdm_codesystems import RARELINK_CDM_V2_0_0_RESOURCES
 from .rarelink_cdm import RARELINK_CDM_V2_0_0
 from .rarelink_cdm_vs import RARELINK_CDM_V2_0_0_VS
 from .rarelink_cdm import load_rarelink_data
-from .rarelink_cdm import rarelink_cdm_phenopackets_mapping
-from .rarelink_cdm import rarelink_cdm_multiple_fields
+from .rarelink_cdm_phenopackets_mapping import rarelink_cdm_phenopackets_mapping
+from .rarelink_cdm_multiple_fields import rarelink_cdm_multiple_fields
 
 __all__ = [
     "RARELINK_CDM_V2_0_0",
