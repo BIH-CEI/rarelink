dfe3e56bfb1cf17495a19932d0c1c8a116c20cdc
graefea <adam.graefe@charite.de>
2024-09-25 11:50:14 +0200

    added functions and import

--- src/rarelink/rarelink_cdm/__init__.py
+++ src/rarelink/rarelink_cdm/__init__.py
@@ -5,10 +5,12 @@ from .rarelink_cdm_codesystems import RARELINK_CDM_V2_0_0_RESOURCES
 from .rarelink_cdm import RARELINK_CDM_V2_0_0
 from .rarelink_cdm_vs import RARELINK_CDM_V2_0_0_VS
 from .rarelink_cdm import load_rarelink_data
+from .rarelink_cdm import rarelink_cdm_phenopacket_mapping
 
 __all__ = [
     "RARELINK_CDM_V2_0_0",
     "RARELINK_CDM_V2_0_0_VS",
     "RARELINK_CDM_V2_0_0_RESOURCES",
     "load_rarelink_data",
+    "rarelink_cdm_phenopacket_mapping"
 ]
--- src/rarelink/rarelink_cdm/rarelink_cdm_phenopacket_mapping.py
+++ src/rarelink/rarelink_cdm/rarelink_cdm_phenopacket_mapping.py
@@ -1,7 +1,40 @@
-# from phenopackets.schema.v2 import Phenopacket
-# from phenopackets.schema.v2 import phenopackets
-# from phenopacket_mapper.mapping.mapper import PhenopacketMapper, PhenopacketElement
-# from rarelink.rarelink_cdm import RARELINK_CDM_V2_0_0
+from phenopackets.schema.v2 import Phenopacket
+from phenopackets.schema.v2 import phenopackets
+from phenopacket_mapper.mapping.mapper import PhenopacketMapper, PhenopacketElement
+from rarelink.rarelink_cdm import RARELINK_CDM_V2_0_0
+
+
+
+def pref_disease_onset(values):
+    date_of_diagnosis, date_of_onset, age_at_onset = values
+    if date_of_diagnosis:
+        return date_of_diagnosis
+    elif date_of_onset:
+        return date_of_onset
+    elif age_at_onset:
+        return age_at_onset
+    else:
+        raise ValueError("All values passed are None")
+        
+def pref_hgvs_code(values):
+    g_HGVS, c_HGVS, p_HGVS = values
+    if g_HGVS:
+        return g_HGVS
+    elif c_HGVS:
+        return c_HGVS
+    elif p_HGVS:
+        return p_HGVS
+    else:
+        raise ValueError("All values passed are None")
+    
+def pref_zygosity_code(values):
+    zygosity, zygosity_other = values
+    if zygosity:
+        return zygosity
+    elif zygosity_other:
+        return zygosity_other
+    else:
+        raise ValueError("All values passed are None")
 
 
 # def map_rarelink_cdm_to_phenopacket(self, data_model: RARELINK_CDM_V2_0_0) -> Phenopacket:
