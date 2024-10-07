35aada629d092247e16fb87f1c9afe80c835709d
frehburg <fil.rehburg@gmail.com>
2024-10-07 12:14:14 +0200

    fixed steps of pipeline

--- src/rarelink/pipelines/redcap_phenopacket_pipeline/phenopacket_pipeline.py
+++ src/rarelink/pipelines/redcap_phenopacket_pipeline/phenopacket_pipeline.py
@@ -1,11 +1,7 @@
 from pathlib import Path
 from typing import Union, List
 from phenopackets.schema.v2 import Phenopacket
-import phenopackets
-from phenopacket_mapper.mapping.mapper import PhenopacketMapper, PhenopacketElement
-from rarelink.preprocessing.preprocess_redcap_for_phenopackets import preprocess_redcap_for_phenopackets
 from rarelink.rarelink_cdm import RARELINK_CDM_V2_0_0
-from rarelink.preprocessing import preprocess_redcap_for_phenopackets
 from rarelink.rarelink_cdm.rarelink_cdm import load_rarelink_data
 from rarelink.rarelink_cdm import rarelink_cdm_phenopackets_mapping
 
@@ -16,18 +12,21 @@ def rarelink_cdm_phenopacket_pipeline(path: Union[str, Path]) -> List[Phenopacke
     # 1. load data
     data_model = RARELINK_CDM_V2_0_0
     data_set = load_rarelink_data(path)
-    print("idsd of the fields in the data model: \n", 
+    print("ids of the fields in the data model: \n",
           data_model.data_model.get_field_ids())
-    # 2. Preprocessing
-    preprocess_redcap_for_phenopackets(data_set)
     
-    # 3. Define the Mapping 
-    rarelink_cdm_phenopackets_mapping(data_set)
+    # 2. Define the Mapping
+    phenopacket_mapper_obj = rarelink_cdm_phenopackets_mapping(data_set)
 
-    # 4. Validate Phenopackets 
+    # 3. Preprocessing
+    # preprocess_redcap_for_phenopackets(data_set)
 
-    # 5. Return Phenopackets
-    return phenopackets
+    # 4. Perform the Mapping
+    phenopackets_list = phenopacket_mapper_obj.map(data_set)
 
-    # return NotImplementedError
+    # 5. Validate Phenopackets
+    # TODO: validate phenopackets
+
+    # 6. Return Phenopackets
+    return phenopackets_list
 
