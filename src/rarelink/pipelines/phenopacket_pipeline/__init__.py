7ccabbbfa789cc4002667700bc5b4b2a4e326c5a
graefea <adam.graefe@charite.de>
2024-12-15 04:21:30 +0300

    fix

--- src/rarelink/pipelines/phenopacket_pipeline/__init__.py
+++ src/rarelink/pipelines/phenopacket_pipeline/__init__.py
@@ -1,7 +1,7 @@
-"""This module implements the pipeline from REDCap data to phenopackets."""
+# """This module implements the pipeline from REDCap data to phenopackets."""
 
-from .phenopacket_pipeline import rarelink_cdm_phenopacket_pipeline
+# from .phenopacket_pipeline import rarelink_cdm_phenopacket_pipeline
 
-__all__ = [
-    "rarelink_cdm_phenopacket_pipeline"
-]
+# __all__ = [
+#     "rarelink_cdm_phenopacket_pipeline"
+# ]
--- src/rarelink_cdm/v2_0_0_dev0/datamodel/__init__.py
+++ src/rarelink_cdm/v2_0_0_dev0/datamodel/__init__.py
@@ -1,17 +1,17 @@
-"""
-Datamodel Pythons for the Rarelink CDM v2.0.0-dev0 based on the Rarelink CDM 
-v2.0.0-dev0 LinkML schema definitions.
-"""
+# """
+# Datamodel Pythons for the Rarelink CDM v2.0.0-dev0 based on the Rarelink CDM 
+# v2.0.0-dev0 LinkML schema definitions.
+# """
 
-from .rarelink_cdm import RarelinkCDM
-# from .rarelink_cdm_codesystems import RarelinkCDMCodeSystems
-# from .rarelink_cdm_entities import RarelinkCDMEntities
-# from .rarelink_cdm_fields import RarelinkCDMFields
+# from .rarelink_cdm import RarelinkCDM
+# # from .rarelink_cdm_codesystems import RarelinkCDMCodeSystems
+# # from .rarelink_cdm_entities import RarelinkCDMEntities
+# # from .rarelink_cdm_fields import RarelinkCDMFields
 
-__all__ = [
-    "RarelinkCDM",
-]
-#     "RarelinkCDMCodeSystems",
-#     "RarelinkCDMEntities",
-#     "RarelinkCDMFields",
+# __all__ = [
+#     "RarelinkCDM",
 # ]
+# #     "RarelinkCDMCodeSystems",
+# #     "RarelinkCDMEntities",
+# #     "RarelinkCDMFields",
+# # ]
