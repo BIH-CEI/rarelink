d45d43efe4b7b233b85c7914d77632cb03fcf039
graefea <adam.graefe@charite.de>
2025-03-16 13:06:47 +0200

    fixed modules for import

--- src/__init__.py
+++ src/__init__.py
@@ -5,6 +5,5 @@ This package contains all submodules and utilities for the RareLink project,
 including the CLI and Common Data Model (CDM) implementations.
 """
 
-
-__all__ = [
-]
+# This file is intentionally left mostly empty.
+# It's needed for Python to recognize the 'src' directory as a package.
