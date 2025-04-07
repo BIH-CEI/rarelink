fc2e01d3c3073b10328a6c5c49c1a89c8ae3e401
graefea <adam.graefe@charite.de>
2025-04-07 12:19:39 +0200

    fixed vital status

--- src/rarelink/phenopackets/mappings/vital_status_mapper.py
+++ src/rarelink/phenopackets/mappings/vital_status_mapper.py
@@ -1,7 +1,7 @@
 from typing import Dict, Any, Optional
 import logging
 from phenopackets import VitalStatus, OntologyClass, TimeElement, Age
-from phenopackets.schema.v2 import VitalStatus as VitalStatusEnum
+from phenopackets import VitalStatus as VitalStatusEnum
 
 from rarelink.utils.field_access import get_multi_instrument_field_value, get_highest_instance
 from rarelink.phenopackets.mappings.base_mapper import BaseMapper
@@ -64,15 +64,61 @@ class VitalStatusMapper(BaseMapper[VitalStatus]):
             highest_instance = None
             patient_status = None
             
-            # Check for repeated elements
+            # Check for repeated elements and get the highest instance
             if "repeated_elements" in data:
+                highest_instances = {}
+                
+                # First, find the highest instance for each instrument
                 for instrument in instruments:
                     highest_instance = get_highest_instance(
                         data.get("repeated_elements", []), instrument)
                     if highest_instance:
-                        patient_status = highest_instance.get(instrument, {})
-                        if patient_status:
-                            break
+                        highest_instances[instrument] = highest_instance
+                
+                # Process the highest instances we found
+                for instrument, instance in highest_instances.items():
+                    # First check if the data is directly in the repeat element
+                    if instrument in instance:
+                        patient_status = instance.get(instrument, {})
+                        break
+                    # Otherwise, check if the data is nested under a different key
+                    # In your case, it seems to be under 'patient_status'
+                    elif "patient_status" in instance:
+                        patient_status = instance.get("patient_status", {})
+                        break
+                    # Check for other common variations of the name
+                    elif instrument.replace("rarelink_3_", "") in instance:
+                        # Sometimes instruments have prefix removed
+                        shortened_name = instrument.replace("rarelink_3_", "")
+                        patient_status = instance.get(shortened_name, {})
+                        break
+                
+                # If no highest instance was found, try a more manual approach
+                if not patient_status:
+                    highest_instance_num = -1
+                    highest_elem = None
+                    
+                    for elem in data.get("repeated_elements", []):
+                        for instrument in instruments:
+                            if elem.get("redcap_repeat_instrument") == instrument:
+                                instance_num = int(elem.get("redcap_repeat_instance", 0))
+                                if instance_num > highest_instance_num:
+                                    highest_instance_num = instance_num
+                                    highest_elem = elem
+                    
+                    if highest_elem:
+                        for instrument in instruments:
+                            # Try the various ways the data might be stored
+                            if instrument in highest_elem:
+                                patient_status = highest_elem.get(instrument, {})
+                                break
+                            elif "patient_status" in highest_elem:
+                                patient_status = highest_elem.get("patient_status", {})
+                                break
+                            elif instrument.replace("rarelink_3_", "") in highest_elem:
+                                shortened_name = instrument.replace("rarelink_3_", "")
+                                patient_status = highest_elem.get(shortened_name, {})
+                                break
             
             # Try direct access if not found in repeated elements
             if not patient_status:
@@ -81,6 +127,15 @@ class VitalStatusMapper(BaseMapper[VitalStatus]):
                         patient_status = data.get(instrument, {})
                         if patient_status:
                             break
+                    # Try with 'patient_status' key
+                    elif "patient_status" in data:
+                        patient_status = data.get("patient_status", {})
+                        break
+                    # Try with shortened name
+                    elif instrument.replace("rarelink_3_", "") in data:
+                        shortened_name = instrument.replace("rarelink_3_", "")
+                        patient_status = data.get(shortened_name, {})
+                        break
             
             # If still no data, use default
             if not patient_status:
@@ -95,11 +150,7 @@ class VitalStatusMapper(BaseMapper[VitalStatus]):
             # Get status value using multi-instrument field access
             status_value = None
             if status_field and status_field != "_default_":
-                status_value = get_multi_instrument_field_value(
-                    data=data,
-                    instruments=instruments,
-                    field_paths=[status_field]
-                )
+                status_value = self._get_field_value(data, instruments, status_field, patient_status)
             
             # Map status to string then to enum value
             if status_value:
@@ -108,11 +159,7 @@ class VitalStatusMapper(BaseMapper[VitalStatus]):
                     
             # Time of death - only add if we have actual data
             if time_of_death_field and dob:
-                death_date = get_multi_instrument_field_value(
-                    data=data,
-                    instruments=instruments,
-                    field_paths=[time_of_death_field]
-                )
+                death_date = self._get_field_value(data, instruments, time_of_death_field, patient_status)
                 
                 if death_date:
                     try:
@@ -124,11 +171,7 @@ class VitalStatusMapper(BaseMapper[VitalStatus]):
 
             # Cause of death - only add if we have actual data
             if cause_of_death_field:
-                cause_value = get_multi_instrument_field_value(
-                    data=data,
-                    instruments=instruments,
-                    field_paths=[cause_of_death_field]
-                )
+                cause_value = self._get_field_value(data, instruments, cause_of_death_field, patient_status)
                 
                 if cause_value:
                     cause_id = self.process_code(cause_value)
@@ -147,6 +190,30 @@ class VitalStatusMapper(BaseMapper[VitalStatus]):
             logger.debug(traceback.format_exc())
             # Return the default VitalStatus on error
             return vital_status
+    
+    def _get_field_value(self, data, instruments, field_path, patient_status):
+        """
+        Helper method to get field value either directly from patient_status or using multi-instrument field access.
+        
+        Args:
+            data: The complete data dictionary
+            instruments: List of instruments to check
+            field_path: Field path to get
+            patient_status: Patient status data dictionary if already found
+            
+        Returns:
+            Field value if found, None otherwise
+        """
+        # First, try to get directly from patient_status if we have it
+        if patient_status and field_path in patient_status:
+            return patient_status.get(field_path)
+        
+        # Otherwise, fall back to the standard field access method
+        return get_multi_instrument_field_value(
+            data=data,
+            instruments=instruments,
+            field_paths=[field_path]
+        )
     
     def _map_multi_entity(self, data: Dict[str, Any], instruments: list, **kwargs) -> list:
         """
