5c08ba6ce84049e2e6368d6dfeefd32a3eed361d
Adam Graefe <adam.graefe@charite.de>
2026-07-28 20:33:20 +0200

    re: #229 added consistent __init__s

---
+++ src/rarelink/phenopackets/mappings/__init__.py
@@ -0,0 +1,6 @@
+"""Phenopacket entity mappers.
+
+Each module maps one Phenopacket block (individual, disease, phenotypic
+feature, measurement, medical action, interpretation, variation descriptor,
+vital status, metadata) from a RareLink-structured record.
+"""
