a8abb003a20938ccbbd041a22134925996dc9820
Adam Graefe <adam.graefe@charite.de>
2026-08-08 21:10:57 +0200

    refactor(#242)

--- src/rarelink/cli/utils/validation_utils.py
+++ src/rarelink/cli/utils/validation_utils.py
@@ -3,7 +3,7 @@ from dotenv import dotenv_values
 from urllib.parse import urlparse
 import json
 import typer
-import subprocess
+#2import subprocess
 from rarelink.cli.utils.string_utils import success_text, error_text, hyperlink
 
 ENV_PATH = Path(".env")
